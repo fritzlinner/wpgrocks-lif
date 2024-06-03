ec2802aac22dc85cfe3cf192ce21b9154488f259
fritzlinner <114403111+fritzlinner@users.noreply.github.com>
2024-06-03 14:36:52 +0000

    grinding tetris game with previews designs and start creating the stuff

--- js/game.mjs
+++ js/game.mjs
@@ -1,14 +1,19 @@
 import { getRand} from './tetrominos.mjs'
-
+import { board } from './board.mjs'
 const rtet= getRand()
+const boardDiv = document.getElementById('board')
+board.showStartScreen(boardDiv)
+const = preViewDiv = document.getElement
 
 for(let i = 0; i < 5; i++) {
 const tet = rtet[i % rtet.length]
 for ( let j = 0; j < tet.length; j++) {
     console.log(tet[j])
+
 }
-console.log('Drehen')
+
 }
+
 const name = 'WPGROCKS'
 
 export {
---
+++ js/preview.mjs
@@ -0,0 +1,3 @@
+const showPreview = (rootElem, tetromino) => {
+    
+}
